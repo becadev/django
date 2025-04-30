392e9e9866d8a02c7f5dba0b15e5513f2e5f5adc
strategi <strategi@pop-os.localdomain>
2025-04-30 16:31:11 -0300

    dados do models

---
+++ galeria/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from galeria.models import Fotografia
+
+def index(request):
+    # Vai trazer a lista de objetos la do models 
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
