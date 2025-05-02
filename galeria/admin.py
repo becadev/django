664c0c71cca9a8aa4ead02319f507a3cd68bb248
strategi <strategi@pop-os.localdomain>
2025-05-02 17:40:31 -0300

    dados feitos

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from galeria.models import Fotografia
 # Register your models here.
+
+# configurações do django admin para melhorar a visualização
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id",  "nome", "legenda")
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+
+admin.site.register(Fotografia, ListandoFotografias)
--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from galeria.models import Fotografia
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
     # Vai trazer a lista de objetos la do models 
     fotografias = Fotografia.objects.all()
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id): # foto_id vai servir para pegar os dados referente ao id dado no bd
+    fotografia = get_object_or_404(Fotografia, pk = foto_id) # metodo proprio do django para mostrar a imagem ou gerar o 4040 em caso de not found
+    return render(request, 'galeria/imagem.html',{"fotografia": fotografia})
